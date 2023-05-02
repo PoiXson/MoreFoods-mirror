ef7827715f7c92305bff606de01a3fd01ad7f928
lorenzop <lorenzo@poixson.com>
2023-05-02 03:43:50 -0400

    fixed and finished MoreFoodsAPI

--- api/src/com/poixson/morefoods/MoreFoodsAPI.java
+++ api/src/com/poixson/morefoods/MoreFoodsAPI.java
@@ -1,5 +1,6 @@
 package com.poixson.morefoods;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
@@ -12,10 +13,12 @@ import org.bukkit.plugin.ServicesManager;
 public class MoreFoodsAPI {
 	protected static final Logger LOG = Logger.getLogger("Minecraft");
 
-	protected static final String NAME  = "Foods";
+	protected static final String NAME  = "MoreFoods";
 	protected static final String CLASS = "com.poixson.morefoods.MoreFoodsPlugin";
 
 	protected final MoreFoodsPlugin plugin;
+
+	protected static final AtomicInteger errcount_PluginNotFound = new AtomicInteger(0);
 
 
 
@@ -31,38 +34,35 @@ public class MoreFoodsAPI {
 		try {
 			if (Class.forName(CLASS) == null)
 				throw new ClassNotFoundException(CLASS);
-		} catch (ClassNotFoundException e) {
-			LOG.severe("Plugin not found: "+NAME);
-			return null;
-		}
-		{
 			final PluginManager manager = Bukkit.getPluginManager();
 			final Plugin plugin = manager.getPlugin(NAME);
+			if (plugin == null) throw new RuntimeException(NAME+" plugin not found");
 			return new MoreFoodsAPI(plugin);
+		} catch (ClassNotFoundException e) {
+			if (errcount_PluginNotFound.getAndIncrement() < 10)
+				LOG.severe("Plugin not found: "+NAME);
+			return null;
 		}
 	}
 
 	protected MoreFoodsAPI(final Plugin p) {
+		if (p == null) throw new NullPointerException();
 		this.plugin = (MoreFoodsPlugin) p;
 	}
 
 
 
 	public boolean isFresh(final ItemStack stack) {
-return false;
-//		return this.plugin.isFresh(stack);
+		return this.plugin.isFresh(stack);
 	}
 	public boolean isUnfresh(final ItemStack stack) {
-return false;
-//		return this.plugin.isUnfresh(stack);
+		return this.plugin.isUnfresh(stack);
 	}
 	public boolean isRotting(final ItemStack stack) {
-return false;
-//		return this.plugin.isRotting(stack);
+		return this.plugin.isRotting(stack);
 	}
 	public boolean isFullyRotten(final ItemStack stack) {
-return false;
-//		return this.plugin.isFullyRotten(stack);
+		return this.plugin.isFullyRotten(stack);
 	}
 
 
